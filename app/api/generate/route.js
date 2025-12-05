b002c21cd8ad22d7e6aa7132b3dcbe8e128ccc22
Vivek <vivekkumarsoni7646@gmail.com>
2025-12-05 23:21:35 +0530

    Fixed some issues

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -4,37 +4,39 @@ import { NextResponse } from "next/server";
 
 export async function POST(request) {
 
-    const body=await request.json()
-    const client=await clientPromise;
-    const db=client.db("snaplink")
-    const collection =db.collection("url")
+    const body = await request.json()
+    const client = await clientPromise;
+    const db = client.db("snaplink")
+    const collection = db.collection("url")
 
-    const link=await collection.findOne({shorturl: body.shorturl})
-    if(link){
-        return NextResponse.json({success:false, error:true, message:"Customized URL already exists!"})
+    const link = await collection.findOne({ shorturl: body.shorturl })
+    if (link) {
+        return NextResponse.json({ success: false, error: true, message: "Customized URL already exists!" })
     }
 
-    let key=body.userkey;
-    const userKey=await collection.findOne({userkey:body.userkey})
+    let key = body.userkey;
 
-    if(userKey){
-        key=userKey.userkey
+    let userDoc = null;
+
+    if (key) {
+        userDoc = await collection.findOne({ userkey: body.userkey })
     }
-    else{
-        key= uuidv4();
+
+    if (!userDoc) {
+        key = uuidv4();
 
         await db.collection('users').insertOne({
-        userkey:key,
-        createdAt:new Date(),
-    });
-    } 
+            userkey: key,
+            createdAt: new Date(),
+        });
+    }
 
-    const result=await collection.insertOne({
-        userkey:key,
-        url:body.url,
-        shorturl:body.shorturl,
-        createdAt:new Date()
+    const result = await collection.insertOne({
+        userkey: key,
+        url: body.url,
+        shorturl: body.shorturl,
+        createdAt: new Date()
     })
 
-    return NextResponse.json({success:true,error:false,message:"URL Generated Successfully!",userkey:key})
+    return NextResponse.json({ success: true, error: false, message: "URL Generated Successfully!", userkey: key })
 }
